04bf15625e1333e22c16b5eb1a511ec7a490ed27
Jeppe Hasseriis <j.hasseriis@gmail.com>
2013-06-19 22:07:20 +0200

    Color change (border, backgrounds). Added footer logos.

--- footer.php
+++ footer.php
@@ -13,13 +13,18 @@
 	  echo wp_nav_menu( $args );
 	  ?>
       <p class="social-links">
-      	<a href="http://soundcloud.com/aphasia-records" target="_blank">Soundcloud</a>
-      	<a href="http://aphasiarecords.bandcamp.com/" target="_blank">Bandcamp</a>
-      	<a href="http://www.facebook.com/AphasiaRecords" target="_blank">Facebook</a>
-      	<a href="http://www.youtube.com/user/AphasiaRecordsMusic" target="_blank">Youtube</a>
-      	<a href="http://twitter.com/AphasiaRecords/" target="_blank">Twitter</a>
+      	<a href="http://soundcloud.com/aphasia-records" target="_blank" class="soundcloud" title="Soundcloud">
+          <img src="<?php echo get_stylesheet_directory_uri() ?>/icons/soundcloud-icon.png" alt="Soundcloud" /></a>
+      	<a href="http://aphasiarecords.bandcamp.com/" target="_blank" class="bandcamp" title="Bandcamp">
+         <img src="<?php echo get_stylesheet_directory_uri() ?>/icons/bandcamp-icon.png" alt="Bandcamp" /></a>
+      	<a href="http://www.facebook.com/AphasiaRecords" target="_blank" class="facebook" title="Facebook">
+         <img src="<?php echo get_stylesheet_directory_uri() ?>/icons/facebook-icon.png" alt="Facebook" /></a>
+      	<a href="http://www.youtube.com/user/AphasiaRecordsMusic" target="_blank" class="youtube" title="Youtube">
+         <img src="<?php echo get_stylesheet_directory_uri() ?>/icons/youtube-icon.png" alt="Youtube" /></a>
+      	<a href="http://twitter.com/AphasiaRecords/" target="_blank" class="twitter" title="Twitter">
+         <img src="<?php echo get_stylesheet_directory_uri() ?>/icons/twitter-icon.png" alt="Twitter" /></a>
       </p>
-      <p>© Copyright 2013 - <a href="http://fister.local/aphasia-records.com/">Aphasia Records</a></p>
+      <p>© Copyright 2013 - <a href="<?php echo home_url( '/' ); ?>">Aphasia Records</a></p>
       <?php //do_action( 'vortex_footer' ); ?>
     </div>
   </div>
