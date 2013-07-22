3ddfbd531eb9ef538e8ae64fd7d326f826e3a210
Jeppe Hasseriis <j.hasseriis@gmail.com>
2013-07-23 00:06:13 +0200

    Added specific styling for artist page. Added header.php with meta tags.

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+    <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+    <meta property="og:url" content="http://www.aphasia-records.com/" />
+    <meta property="og:title" content="Aphasia Records" />
+    <meta property="og:image" content="http://aphasia-records.com/img/og-image.jpg" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="300" />
+
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php wp_head(); ?> 
+</head>
+<body <?php body_class(); ?>>
+<div class="wrapper">  
+  <div id="header">  
+      
+      <div class="container_16 container_header_top clearfix">
+        <div class="grid_16">
+		  <?php get_template_part( 'custom', 'header' ); ?>
+        </div>
+      </div>
+      
+      <div id="nav">
+        <div class="container_16 clearfix">
+          <div class="grid_16">
+            <?php vortex_primary_menu(); ?>
+          </div>
+        </div>
+      </div>      
+  
+  </div>
